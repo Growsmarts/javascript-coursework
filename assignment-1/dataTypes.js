b903db71da9b5740a34f747a115ceab17494c321
Jacob Madaras <jmadar1@live.spcollege.edu>
2026-01-28 13:27:03 -0500

    Add Task 1.1 primitive data types

---
+++ assignment-1/dataTypes.js
@@ -0,0 +1,26 @@
+// This variable is for age being a number
+let age = 25;
+
+// This variable is a string holding my full name
+let fullName = "Jacob Madaras";
+
+// This variable notes if I am currently enrolled in a course
+let isEnrolled = true;
+
+// This variable is declared and not given a value
+let futureGoal;
+
+// This variable is set to null because the course is not finished
+let completedCourse = null;
+
+// Print
+console.log("Age:", age, "Type:", typeof age);
+console.log("Name:", fullName, "Type:", typeof fullName);
+console.log("Enrolled:", isEnrolled, "Type:", typeof isEnrolled);
+console.log("Future Goal:", futureGoal, "Type:", typeof futureGoal);
+console.log("Completed Course:", completedCourse, "Type:", typeof completedCourse);
+
+// What I learned:
+// Undefined means a variable exists but hasn’t been given a value yet.
+// Null means the variable is by choice empty.
+// I would use undefined when something hasn’t been set and null when I want to explicitly say there isn't a value.
